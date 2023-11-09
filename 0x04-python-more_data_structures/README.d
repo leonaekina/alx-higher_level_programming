a58cd4a58f093b5e61440fc16015e901347423b6
leonaekina <leonaekina@gmail.com>
2023-11-09 03:41:32 -0800

    README

---
+++ 0x04-python-more_data_structures/README.d
@@ -0,0 +1,15 @@
+0x04 Python - More Data Structures: Set, Dictionary
+This project covers data structures in Python (set, dictionaries)
+
+At the end of this project, I was able to solve these questions:
+
+What are set and how to use them
+What are the most common methods of set and how to use them
+When to use sets versus lists
+How to iterate into a set
+What are dictionary and how to use them
+When to use dictionaries versus lists or sets
+What is a key in a dictionary
+How to iterate into a dictionary
+What is a lambda function
+What is map, reduce and map functions
